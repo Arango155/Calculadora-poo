b1e3055acd5281bb4d9a7f1721c6edc00ae14a15
Arango155 <jdarango031@gmail.com>
2022-05-21 09:05:27 -0600

    Agrego vectores y matrices

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -1,95 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package calculadora;
 
 import java.util.Scanner;
 
-/**
- *
- * @author jdara
- */
 public class Calculadora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     
-     float a,b;
-     float x,y;
+     float a,b,x,y;
     
-    Scanner entrada=new Scanner(System.in);
+    Scanner entrada=new Scanner(System.in);    
     
-    System.out.println("Bienvenido a la calculadora para la funcion de suma, resta, multiplicacion, division, potencia y raiz de los numeros que ingrese");
-    System.out.println("Se le solicita que ingrese los numeros que se le piden acontinuacion");
+    int [] vector = {1,2};
+    float suma = 0,resta = 0, multiplicacion=1,division=1, potencia = 0, raiza = 0,raizb = 0;
     
-    System.out.println("Ingrese el primer numero: ");
-    a=entrada.nextFloat();
+    for (int i=0; i<vector.length; i++) {
+            System.out.println("Ingrese un numero: ");
+            vector[i] = entrada.nextInt();
+            
+            suma=suma+vector[i];
+            resta=vector[0]-vector[1];
+            multiplicacion=multiplicacion*vector[i];
+            division=vector[0]/vector[1];
+            potencia= (int) Math.pow(vector[0],vector[1]);
+            raiza= (int) Math.sqrt(vector[0]);
+            raizb= (int) Math.sqrt(vector[1]);     
+        }
+        
+        System.out.println("la suma dada es: "+ suma);
+        System.out.println("la resta dada es: "+ resta);
+        System.out.println("la multiplicacion dada es: "+multiplicacion);
+        System.out.println("la division dada es: "+ division);
+        System.out.println("la potencia dada es: "+potencia);
+        System.out.println("la raiz cuadrada del primer numero es: "+raiza);
+        System.out.println("la raiz cuadrada del segundo numero es: "+raizb);
     
-    System.out.println("Ingrese el segundo numero: ");
-    b=entrada.nextFloat();
     
-    Proyecto calculadora=new Proyecto();
+
     
-    calculadora.suma(a, b);
-    
-    System.out.println("El valor de la suma entra ambos numeros es:");
-    System.out.println(calculadora.suma(a, b));
-    
-    System.out.println("El valor de la resta entre ambos numeros es:");
-     System.out.println(calculadora.resta(a, b));
-     
-     System.out.println("El valor de la multiplicacion entre ambos numeros es:");
-     System.out.println(calculadora.multi(a, b));
-     
-       System.out.println("El valor de la division entre ambos numeros es:");
-     System.out.println(calculadora.div(a, b));
-    
-     System.out.println("El valor de la potencia del primer numero es de:");
-     System.out.println(calculadora.pota(a));
-    
-     System.out.println("El valor de la potencia del segundo numero es de:");
-     System.out.println(calculadora.potb(b));
-    
-     System.out.println("El valor de la raiz cuadrada del primer numero es de:");
-     System.out.println(calculadora.raiza(a));
-     
-      System.out.println("El valor de la raiz cuadrada del segundo numero es de:");
-     System.out.println(calculadora.raizb(b));
-     
-     System.out.println("Ahora se haran algunas operaciones especiales en la calculadora ");
-     
-    System.out.println("Se le solicita que ingrese los numeros que se le piden acontinuacion");
-    
-    System.out.println("Ingrese el primer numero (base): ");
-    x=entrada.nextFloat();
-    
-    System.out.println("Ingrese el segundo numero (exponente): ");
-    y=entrada.nextFloat();
-    
-        System.out.println("El valor de la potencia de la base de su primer numero y el exponente del segundo es:");
-     System.out.println(calculadora.pot(x,y));
-         
-        System.out.println("El valor de la raiz cuadrada del primer numero es de:");
-     System.out.println(calculadora.raizx(x));
-     
-      System.out.println("El valor de la raiz cuadrada del segundo numero es de:");
-     System.out.println(calculadora.raizy(y));
-     
-     System.out.println("El valor random de su numero random es:");
-     System.out.println(calculadora.randomx(x));
-     
-     
-     System.out.println("El primer numero multiplicado por pi es:");
-     System.out.println(calculadora.pi(x));
-    
-  
     }
-    
-    
-    
-   
-}
+}        
+
+  
--- calculadora/Clase.java
+++ calculadora/Clase.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadora;
 
-/**
- *
- * @author jdara
- */
-public class Proyecto {
+public class Clase {
    
     
-    public Proyecto(){
+    public Clase(){
 }
     
-    
     public float suma (float a, float b){
+        
         float suma= a+b; 
         return suma;
     }
@@ -57,6 +49,10 @@ public class Proyecto {
         return raizb;
     }
       
+        
+        
+      
+             
          public float randomx (float x){
         float randomx= (float) (Math.random()*10); 
      
@@ -77,7 +73,7 @@ public class Proyecto {
     }
         
         public float pot (float x, float y){
-        float pot= (float) Math.pow(x,y);
+        float pot= (float) Math.pow(x, y);
      
         return pot;
     }
